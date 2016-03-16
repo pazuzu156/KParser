e1f8e6c248a5e95eaca7206c8ea2d89ce6dc7d29
Kaleb Klein <klein.jae@gmail.com>
2016-03-16 18:34:24 -0400

    Added PHPUnit testing for travis

---
+++ tests/ParsingTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Pazuzu156\KParser\KParser;
+
+class ParsingTest extends PHPUnit_Framework_TestCase
+{
+    public function testParse()
+    {
+        $parser = new KParser;
+        $this->assertTrue($parser->parse("Testing parsing method") !== '');
+    }
+}
